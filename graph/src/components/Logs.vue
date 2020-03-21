1bb46a309487a949f4429790befd6921c622985e
Artem Zaglubotskii <mr_a953@mail.ru>
2020-03-22 00:58:06 +0300

    create template of Logs page

--- graph/src/components/Logs.vue
+++ graph/src/components/Logs.vue
@@ -1,13 +1,81 @@
 <template>
-  <div>
+  <div class="container">
     <h1>Logs</h1>
+    <ul class="nav nav-tabs">
+      <li role="presentation" @click="currentTab='Alarms'">
+        <a href="#">Alarms</a>
+      </li>
+      <li role="presentation" @click="currentTab='Logs'">
+        <a href="#">Logs</a>
+      </li>
+    </ul>
+    <div class="tab-content">
+      <div v-if="currentTab == 'Alarms'">
+      <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Datetime</th>
+              <th scope="col">Info</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="inf in alarms" :key="inf.id">
+              <td>{{ inf.datetime }}</td>
+              <td>{{ inf.Info}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div v-if="currentTab == 'Logs'">
+      <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Datetime</th>
+              <th scope="col">Info</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="inf in logs" :key="inf.id">
+              <td>{{ inf.datetime }}</td>
+              <td>{{ inf.Info}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
 
   </div>
 </template>
 
 <script>
     export default {
-        name: "Logs"
+       data() {
+         return {
+           alarms: [{
+             "id": 0,
+             "datetime": "22.03.2020",
+             "Info": "Alarm"
+           }],
+           logs: [{
+             "id": 0,
+             "datetime": "21.03.2020",
+             "Info": "Logs"
+           }
+           ],
+           chooser: ['Alarms','Log'],
+           currentTab: "Alarms",
+         }
+       },
+      methods: {
+        getAlarms() {
+
+        },
+        getLogs(){
+
+        }
+      }
 
     }
 </script>
