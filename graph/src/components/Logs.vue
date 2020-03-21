925723c3cee4b577ea14954ccfdb029c58e4eda6
Artem Zaglubotskii <mr_a953@mail.ru>
2020-03-21 18:32:24 +0300

    add node id

---
+++ graph/src/components/Logs.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Logs</h1>
+
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Logs"
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ graph/src/components/Settings.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Settings</h1>
+    <hr><br>
+    <button type="button" class="btn btn-success btn-sm" @click="" v-b-modal>New Settings</button>
+  </div>
+</template>
+
+<script>
+    export default {
+        data() {
+          return {
+
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
