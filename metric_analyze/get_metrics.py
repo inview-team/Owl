afef20d16bc5881b822cc733ccea8318a3ae57c2
f0m41h4u7 <fomalhaut_a3v@outlook.com>
2020-03-25 18:14:34 +0000

    update gitlabci

--- metric_analyze/get_metrics.py
+++ metric_analyze/get_metrics.py
@@ -2,9 +2,24 @@ from clickhouse_driver import Client
 import json
 import requests
 
-nodes = {"ns=2;i=9": "pressure", "ns=2;i=10": "humidity", "ns=2;i=11": "roomTemperature", "ns=2;i=12": "workingAreaTemperature", "ns=2;i=13": "pH", "ns=2;i=14": "weight", "ns=2;i=15": "fluidFlow", "ns=2;i=16": "co2"}
+nodes = {"ns=2;i=9": "Pressure", "ns=2;i=10": "humidity", "ns=2;i=11": "roomTemperature", "ns=2;i=12": "workingAreaTemperature", "ns=2;i=13": "pH", "ns=2;i=14": "weight", "ns=2;i=15": "fluidFlow", "ns=2;i=16": "co2"}
 min = {"pressure": 1001, "humidity": 25, "roomTemperature":15, "workingAreaTemperature":200, "pH":3, "weight":100, "fluidFlow":10, "co2":0}
 max = {"pressure": 1099, "humidity": 40, "roomTemperature":46, "workingAreaTemperature":500, "pH":8, "weight":900, "fluidFlow":50, "co2":30}
+
+test_min={}
+test_max={}
+
+getData=False
+while !getData
+    try:
+        r = requests.get(url='http://127.0.0.1:5000/settings')
+        result= r.json()
+        settings = result['settings']
+        for i in range(len(settings)):
+            test_min[settings[i]['metric']] = settings[i]['from']
+            test_max[settings[i]['metric']] = settings[i]['to']
+        getData=True
+
 
 while True:
     client = Client('clickhouse-svc')
