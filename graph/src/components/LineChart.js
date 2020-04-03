931d107494cfaff2e57a7bdb0eb3e6e868568fb1
Zevsoverlord <mr_a953@mail.ru>
2020-04-03 21:37:08 +0300

    render line chart javascript

---
+++ graph/src/components/LineChart.js
@@ -0,0 +1,39 @@
+import { Line, mixins } from 'vue-chartjs';
+
+const { reactiveProp } = mixins;
+
+export default {
+  extends: Line,
+  mixins: [reactiveProp],
+  data() {
+    return {
+      options: { // Chart.js options
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+            },
+            gridLines: {
+              display: true,
+            },
+          }],
+          xAxes: [{
+            gridLines: {
+              display: false,
+            },
+          }],
+        },
+        legend: {
+          display: true,
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    };
+  },
+  mounted() {
+    // this.chartData создаётся внутри миксина.
+    // Если вы хотите передать опции, создайте локальный объект options
+    this.renderChart(this.chartData, this.options);
+  },
+};
