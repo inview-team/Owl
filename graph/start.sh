b4f9453bcd5e47505dcdd160a3dfae545d83ce8a
f0m41h4u7 <fomalhaut_a3v@outlook.com>
2020-04-18 23:38:38 +0000

    restapi address

--- graph/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "RESTAPI_SVC_SERVICE_HOST=" > /tmp/nginx/frontend/.env
-addr=$(echo $RESTAPI_SVC_SERVICE_HOST)
-sed '$ s/$/'$addr'/' /tmp/nginx/frontend/.env > /tmp/nginx/frontend/.env.new && mv /tmp/nginx/frontend/.env.new /tmp/nginx/frontend/.env
-nginx -g "daemon off;"
